1aa730b7757747af8915c0350e0b74cd8f440458
matosluizdev <luizmatosdev@outlook.com>
2024-01-06 17:56:46 -0300

    feat: como passar os dados do controller para os componentes vue na view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-       return inertia('Index/Index');
+       return inertia('Index/Index',
+       [
+        'message' => 'Hello World!'
+       ]
+    );
+
     }
 
     public function show() {
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,14 @@
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    defineProps({
+        message: String
+    })
 </script>
