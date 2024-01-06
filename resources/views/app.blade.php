a78e7039ff00bef7c39dbc635079442a83d724fd
matosluizdev <luizmatosdev@outlook.com>
2024-01-06 17:44:37 -0300

    feat: Entendo fluxo de rota com inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>LaraLouis</title>
-        @vite('resources/js/app.js');
+        @vite('resources/js/app.js')
         @inertiaHead
     </head>
     <body>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/hello', [IndexController::class, 'show']);
