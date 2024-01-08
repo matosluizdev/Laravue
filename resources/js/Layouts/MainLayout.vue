b1c4c164d019ea8dce27972379a1025927817ad5
matosluizdev <luizmatosdev@outlook.com>
2024-01-07 23:52:49 -0300

    feat: ilustrando a reatividade no vue com um contador simples

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,13 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <slot></slot>
+    <div>Contador: {{ timer }} </div>
+    <slot>Valor padrão caso falhe</slot>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue'
+    const timer = ref(0)
+    setInterval(() => timer.value++, 1000)
 </script>
