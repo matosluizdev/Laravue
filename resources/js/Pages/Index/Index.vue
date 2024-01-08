a941da02676806147457b416e0c48ea54adf7457
matosluizdev <luizmatosdev@outlook.com>
2024-01-07 23:17:18 -0300

    feat: Layouts utilizando slots

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is {{ message }}
-    </div>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>
+            The message is {{ message }}
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue';
     defineProps({
         message: String
     })
