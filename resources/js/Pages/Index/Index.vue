603e79816264f9be384431e56acf93dce51b729d
matosluizdev <luizmatosdev@outlook.com>
2024-01-08 22:30:45 -0300

    feat: Layouts padrão, eliminando uma etapa para manter o estado do componente

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -8,15 +8,4 @@
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    defineProps({
-        message: String
-    })
-</script>
-
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    export default  {
-        layout: MainLayout
-    }
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -5,12 +5,6 @@
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
-import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
 
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    export default  {
-        layout: MainLayout
-    }
-</script>
+
