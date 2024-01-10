3259eb2e7f5f1dbdd6da796b61d7d16e4c284b5a
matosluizdev <luizmatosdev@outlook.com>
2024-01-10 19:07:11 -0300

    feat: criando Factory para nossa tabela de listings

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'beds' => fake()->numberBetween(1,7),
+            'baths' => fake()->numberBetween(1,7),
+            'area' => fake()->numberBetween(30,400),
+            'city' => fake()->city(),
+            'code' => fake()->postcode(),
+            'street' => fake()->streetName(),
+            'street_number' => fake()->numberBetween(10,300),
+            'price' => fake()->numberBetween(50_000, 2_000_000)
+
+        ];
+    }
+}
