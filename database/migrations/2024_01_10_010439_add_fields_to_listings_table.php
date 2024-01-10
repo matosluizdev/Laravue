8a6b0e02037a532dec99f8a7ed17c16aa1850408
matosluizdev <luizmatosdev@outlook.com>
2024-01-09 22:17:25 -0300

    feat: adicionando campos na tabela listings

---
+++ database/migrations/2024_01_10_010439_add_fields_to_listings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_number');
+
+            $table->unsignedInteger('price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->dropColumns('listings', [
+                'beds', 'baths', 'area', 'city', 'code', 'street', 'street_number', 'price'
+            ]);
+        });
+    }
+};
