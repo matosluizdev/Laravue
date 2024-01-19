c6d4be269e162ee2c16ce7170d3c0151e694825a
matosluizdev <luizmatosdev@outlook.com>
2024-01-19 17:12:03 -0300

    feat: conceito de resource controller e route model binding

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)->only('index', 'show');
