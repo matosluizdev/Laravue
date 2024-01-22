0ec90e9e2ace64695b39eefd3fba1baabe9314fc
matosluizdev <luizmatosdev@outlook.com>
2024-01-22 17:01:58 -0300

    feat: computed properties, exibindo flashmessage

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,30 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>Contador: {{ timer }} </div>
+    <!-- <div>Contador: {{ timer }} </div> -->
+    <div v-if="flashSuccess" class="success">
+{{ flashSuccess }}
+    </div>
+
     <slot>Valor padrão caso falhe</slot>
 </template>
 
 <script setup>
-    import {Link} from '@inertiajs/vue3'
-    import {ref} from 'vue'
-    const timer = ref(0)
-    setInterval(() => timer.value++, 1000)
+    import {Link, usePage} from '@inertiajs/vue3'
+    import {computed} from 'vue';
+
+    const page = usePage()
+    const flashSuccess = computed(
+        () => page.props.flash.success,
+    )
+    // import {ref} from 'vue'
+    // const timer = ref(0)
+    // setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
