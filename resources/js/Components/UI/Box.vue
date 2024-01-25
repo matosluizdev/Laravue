5c7c5b7988658de6342f4d0a1b22a9ea1f28c86e
matosluizdev <luizmatosdev@outlook.com>
2024-01-25 16:18:32 -0300

    feat: estilizando a listagem

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-4 border border-gray-300 rounded-md shadow-sm dark:border-gray-700 dark:text-gray-300">
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+    <Box v-for="listing in listings" :key="listing.id">
     <div>
         <Link :href="route('listing.show', listing.id)">
                 <ListingAddress :listing="listing" />
@@ -11,13 +12,15 @@
     <div>
         <Link :href="route('listing.destroy', listing.id)" method="DELETE">Excluir</Link>
     </div>
-    </div>
+    </Box>
+</div>
 </template>
 
 <script setup>
 
 import { Link } from '@inertiajs/vue3';
-import ListingAddress from '../../Components/ListingAddress.vue';
+import ListingAddress from '@/Components/ListingAddress.vue';
+import Box from '@/Components/UI/Box.vue';
 defineProps({
     listings: Array,
 })
