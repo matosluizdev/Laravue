7361ded497bf6c09aeb31c5352f4e82a4c77f8b1
matosluizdev <luizmatosdev@outlook.com>
2024-01-27 22:16:02 -0300

    feat: adicionando biblioteca de progressbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,16 @@
 import { createApp, h } from 'vue'
 import { ZiggyVue } from 'ziggy'
+import {InertiaProgress} from '@inertiajs/progress'
 import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from './Layouts/MainLayout.vue'
 import '../css/app.css'
+
+InertiaProgress.init({
+    delay: 0,
+    color: '#29d',
+    includeCSS: true,
+    showSpinner:true,
+})
 
 createInertiaApp({
   resolve: name => {
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
 
         <title>LaraLouis</title>
         @routes
