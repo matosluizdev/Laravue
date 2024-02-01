d4aaf692711f3958dcf52dc13edf9e048a7ac9d5
matosluizdev <luizmatosdev@outlook.com>
2024-01-31 23:58:08 -0300

    feat: mostrando o valor aproximado de parcela pra cada casa na listagem inicial

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,30 +1,11 @@
 <template>
 <div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-    <Box v-for="listing in listings" :key="listing.id">
-    <div>
-        <Link :href="route('listing.show', listing.id)">
-            <Price :price="listing.price" class="text-2xl" />
-            <ListingSpace :listing="listing" class="text-lg" />
-            <ListingAddress :listing="listing" class="text-gray-500" />
-        </Link>
-    </div>
-    <div>
-        <Link :href="route('listing.edit', listing.id)">Editar</Link>
-    </div>
-    <div>
-        <Link :href="route('listing.destroy', listing.id)" method="DELETE">Excluir</Link>
-    </div>
-    </Box>
+    <Listing v-for="listing in listings" :key="listing.id" />
 </div>
 </template>
 
 <script setup>
-
-import { Link } from '@inertiajs/vue3';
-import ListingAddress from '@/Components/ListingAddress.vue';
-import Box from '@/Components/UI/Box.vue';
-import ListingSpace from '@/Components/ListingSpace.vue';
-import Price from '../../Components/Price.vue';
+import Listing from './Index/Components/Listing.vue';
 defineProps({
     listings: Array,
 })
---
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -0,0 +1,40 @@
+<template>
+    <Box>
+    <div>
+        <Link :href="route('listing.show', listing.id)">
+            <div class="flex items-center">
+                <Price :price="listing.price" class="text-2xl" />
+                <div class="gap-2 text-xs text-gray-500">
+                    <Price :price="monthlyPayment" />
+                </div>
+            </div>
+
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray-500" />
+        </Link>
+    </div>
+    <div>
+        <Link :href="route('listing.edit', listing.id)">Editar</Link>
+    </div>
+    <div>
+        <Link :href="route('listing.destroy', listing.id)" method="DELETE">Excluir</Link>
+    </div>
+    </Box>
+</template>
+
+<script setup>
+
+import { Link } from '@inertiajs/vue3';
+import ListingAddress from '@/Components/ListingAddress.vue';
+import Box from '@/Components/UI/Box.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
+import Price from '@/Components/Price.vue'
+const props = defineProps({
+    listings: Object,
+})
+const {monthlyPayment} = useMonthlyPayment(
+    props.listing.price, 2.5, 25,
+)
+
+import {useMonthlyPayment} from '@/Composables/useMonthlyPayment'
+</script>
