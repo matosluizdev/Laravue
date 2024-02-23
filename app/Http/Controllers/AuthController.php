e5568fe8e4a4f03b970ff2b419e38e7b494fe688
matosluizdev <luizmatosdev@outlook.com>
2024-02-23 13:01:44 -0300

    feat: criando a parte visual do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function create(){
-
+        return inertia('Auth/Login');
     }
 
     public function store() {
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="login">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email" class="label"> E-mail (username)</label>
+                <input type="text" id="email" class="input" v-model="form.email">
+                <div class="input-error">Potential Errors</div>
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label"> Password</label>
+                <input type="text" id="password" class="input" v-model="form.password">
+            </div>
+            <div class="mt-4 ">
+                <button type="submit" class="w-full btn-primary">Login</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+    import { useForm } from '@inertiajs/vue3'
+
+    const form = useForm({
+        email: null,
+        password: null
+    })
+
+    const login = () => form.post(route('login.store'))
+</script>
