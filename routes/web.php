7b364a5b378cd7006c7ccace86cf2a6d70ad53e2
matosluizdev <luizmatosdev@outlook.com>
2024-02-23 12:44:49 -0300

    feat: criando controller de autenticação e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Models\Listing;
@@ -9,3 +10,10 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+->name('login');
+Route::post('login', [AuthController::class, 'store'])
+->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+->name('logout');
